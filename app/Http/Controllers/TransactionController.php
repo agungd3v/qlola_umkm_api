0a01c73add3bdff228cab559354040b28a137327
agungd3v <agungd3v@gmail.com>
2024-05-23 23:14:51 +0700

    fix counting transaction owner

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -24,17 +24,17 @@ class TransactionController extends Controller
 			if ($userRole != "owner") throw new \Exception("Tidak ada transaksi");
 
 			$transactionToday = $this->user->business->transactions()
-				->whereDate("created_at", Carbon::today())
-				->sum("grand_total");
+				->whereDate("created_at", Carbon::today());
 
 			$transactionMonth = $this->user->business->transactions()
 				->whereYear("created_at", Carbon::now()->year)
-				->whereMonth("created_at", Carbon::now()->month)
-				->sum("grand_total");
+				->whereMonth("created_at", Carbon::now()->month);
 
 			return response()->json([
-				"today" => $transactionToday,
-				"month" => $transactionMonth
+				"transaction_nominal_today" => $transactionToday->sum("grand_total"),
+				"transaction_count_today" => $transactionToday->count(),
+				"transaction_nominal_month" => $transactionMonth->sum("grand_total"),
+				"transaction_count_month" => $transactionMonth->count()
 			]);
 		} catch (\Exception $e) {
 			return response()->json(["message" => $e->getMessage()], 400);
